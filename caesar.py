764759418226521b10acbfb845e3aad175f0eabd
bronsonnaylor <49175664+bronsonnaylor@users.noreply.github.com>
2019-05-19 23:10:39 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,5 +1,29 @@
-from helpers import alphabet_position, rotate_character
+def alphabet_position(letter):
+    compare = ord("a")
+    number = ord(letter.lower()) - compare
+    return number
+
+def rotate_character(char, rot):
+    if char.isalpha():
+        char_number = (alphabet_position(char) + rot) % 26
+        if ord(char) > ord("Z"):
+            character = chr(char_number + ord("a"))
+        else:
+            character = chr(char_number + ord("A"))
+    else:
+        character = char
+    print(char_number)
+    return character
 
 def encrypt(text, rot):
-    #Add your code here
-    return
+    encryption = ""
+    for i in text:
+        encryption += rotate_character(i, rot)
+    return encryption
+
+
+def main():
+    print(encrypt(input("What message would you like to encrypt?"), int(input("How many rotations?"))))
+
+if __name__ == "__main__":
+    main()
